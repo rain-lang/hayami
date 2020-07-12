65411cb65ff7d382e94a663d57e768ebc1dcbe9c
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-12 19:44:06 -0400

    Bench

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,14 +1,24 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{thread_rng, Rng};
 use hayami::SymbolTable;
-use std::collections::HashMap;
+use ahash::HashMap;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = thread_rng();
     let mut table = SymbolTable::new();
     let mut hash_table = HashMap::new();
-    c.bench_function("rand level 0 insertion", |b| {
+    c.bench_function("SymbolTable: level 0 insertion", |b| {
+        let key = rng.gen::<usize>();
+        let value = rng.gen::<usize>();
         b.iter(|| {
+            table.insert(key, value)
+        })
+    });
+    c.bench_function("ahash::HashMap: insertion", |b| {
+        let key = rng.gen::<usize>();
+        let value = rng.gen::<usize>();
+        b.iter(|| {
+            hash_table.insert(key, value)
         })
     });
 }
