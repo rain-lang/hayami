2f029532e5900a5f0373374c3e4bb0ccfdb3242d
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-15 21:16:11 -0400

    Benchmark improvement

--- benches/comparison/main.rs
+++ benches/comparison/main.rs
@@ -4,14 +4,34 @@ Compare `SymbolTable` performance to other `HashMap`s
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{thread_rng, Rng};
-use hayami::SymbolTable;
 use std::collections::HashMap;
 use ahash::RandomState;
 use fxhash::FxHashMap;
 use indexmap::IndexMap;
+use hayami::SymbolTable;
+use im;
+use im_rc;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = thread_rng();
+    let mut im_table = im::HashMap::<usize, usize, RandomState>::default();
+    c.bench_function("im::HashMap: insertion", |b| {
+        let key = rng.gen::<usize>();
+        let value = rng.gen::<usize>();
+        b.iter(|| {
+            im_table.insert(key, value)
+        })
+    });
+    std::mem::drop(im_table);
+    let mut im_rc_table = im_rc::HashMap::<usize, usize, RandomState>::default();
+    c.bench_function("im_rc::HashMap: insertion", |b| {
+        let key = rng.gen::<usize>();
+        let value = rng.gen::<usize>();
+        b.iter(|| {
+            im_rc_table.insert(key, value)
+        })
+    });
+    std::mem::drop(im_rc_table);
     let mut table = SymbolTable::new();
     c.bench_function("SymbolTable: level 0 insertion", |b| {
         let key = rng.gen::<usize>();
