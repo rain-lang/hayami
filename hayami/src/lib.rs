27e604fc55a6c97930f704e8dfbad00cb01fa198
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-18 21:20:09 -0400

    Factored out local symboltable

--- hayami/src/lib.rs
+++ hayami/src/lib.rs
@@ -6,8 +6,6 @@ use std::hash::Hash;
 
 #[cfg(feature = "fast")]
 pub mod fast;
-#[cfg(feature = "local")]
-pub mod local;
 
 pub use symbolmap_trait::*;
 
@@ -17,19 +15,3 @@ mod testing;
 /// The default random state in use
 #[allow(unused)]
 type RandomState = ahash::RandomState;
-
-/// The `Arc` in use
-///
-/// Supports `elysees` (default) or `std`
-#[cfg(feature = "elysees")]
-#[allow(unused)]
-type Arc<T> = elysees::Arc<T>;
-#[cfg(not(feature = "elysees"))]
-#[allow(unused)]
-type Arc<T> = std::sync::Arc<T>;
-
-/// The `Rc` in use
-///
-/// Supports `std` only as of now
-#[allow(unused)]
-type Rc<T> = std::rc::Rc<T>;
